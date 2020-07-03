c52f55138629691b1c632b801395cd24a558f938
Johnathon Kemp <kemp133@googlemail.com>
2020-07-03 15:37:06 +0100

    Added a new view to start having the ability to add new blog posts. Updated urls.py to allow people to get to this URL. Added code to views.py to allow loading and saving of the form data. Added style.css file which will store all custom CSS I need. Updated base.html file to include the new stylesheet.

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index_view, name='blog-index_view'),
+    path('posts/new/', views.blog_new_view, name='blog-new-view')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .forms import CreatePost
 
 
 # Create your views here.
 def index_view(request):
     return render(request, 'blog/index.html', {'title': 'Blog'})
+
+
+def blog_new_view(request):
+    if request.method == "POST":
+        form = CreatePost(request.POST)
+
+        if form.is_valid():
+            data = request.POST.copy()
+
+            print(data.get('blog_title'), data.get('body'))
+            return redirect('blog-index_view')
+    else:
+        form = CreatePost()
+        return render(request, 'blog/new_post.html', {'title': 'New Post', 'form': form})
