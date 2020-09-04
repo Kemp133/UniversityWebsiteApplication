dc7c05b299ede031e1b2193872629008e6bae1a1
Johnathon Kemp <kemp133@googlemail.com>
2020-09-05 00:40:21 +0100

    Modified tests.py in functional_tests to be as it is in the online tutorial, modified views.py to fix a test that had broken in the process

--- cv/views.py
+++ cv/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-	return render(request, "cv/index.html")
+	return render(request, "cv/index.html", {'title': 'My CV'})
--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -6,7 +6,7 @@ import unittest
 import time
 
 
-class LoadCVTest(unittest.TestCase):
+class LoadCVTest(LiveServerTestCase):
 	def setUp(self) -> None:
 		# Get a window to test in
 		self.browser = webdriver.Firefox()
@@ -16,7 +16,7 @@ class LoadCVTest(unittest.TestCase):
 
 	def test_can_load_cv_page(self):
 		# The user loads up the website and goes to the cv section
-		self.browser.get('http://localhost:8000/cv')
+		self.browser.get(f'{self.live_server_url}/cv')
 
 		# Check that CV is mentioned in the title
 		self.assertIn('CV', self.browser.title)
