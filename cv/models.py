3ec3b611392a81dd0129fe6a16745102b560d590
Johnathon Kemp <kemp133@googlemail.com>
2020-09-06 09:52:50 +0100

    Added bare minimum to index.html to pass the previous tests. Added more tests to tests.py to test for the rest of the tables required in the cv (skills, hobbies and past experience). Started adding models to models.py to begin populating the education section dynamically. Furthered the tests in functional_tests to test that all required information is on the page.

--- cv/models.py
+++ cv/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
+
 # Create your models here.
+class Subject(models.Model):
+	class Grades(models.TextChoices):
+		A_STAR = 'A*', 'A*'
+		A = 'A', 'A'
+		B = 'B', 'B'
+		C = 'C', 'C'
+		D = 'D', 'D'
+		E = 'E', 'E'
+		F = 'F', 'F'
+		FAIL = 'FAIL', 'FAIL'
+		NONE = 'N/A', 'N/A'
+
+	name = models.CharField(max_length=64)
+	predicted_grade = models.CharField(max_length=4, choices=Grades.choices, default=Grades.NONE)
+	actual_grade = models.CharField(max_length=4, choices=Grades.choices, default=Grades.NONE)
+	active = models.BooleanField(default=True)
+
+
+class Institution(models.Model):
+	name = models.CharField(max_length=256)
+	address_line_1 = models.CharField(max_length=256)
+	address_line_2 = models.CharField(max_length=256, blank=True)
+	address_line_3 = models.CharField(max_length=256)
+	address_line_4 = models.CharField(max_length=256)
+	post_code = models.CharField(max_length=8)
+
+
+class Education(models.Model):
+	institution = models.ForeignKey(Institution, models.CASCADE)
+	subject = models.ForeignKey(Subject, models.CASCADE)
--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.remote.webelement import WebElement
 from selenium.common.exceptions import NoSuchElementException
 
 from django.test import LiveServerTestCase
@@ -15,6 +16,12 @@ class LoadCVTest(LiveServerTestCase):
 	def tearDown(self) -> None:
 		self.browser.quit()
 
+	def rows_exist_in_table(self, table: WebElement, min_num_of_rows: int = 0, error_msg: str = None):
+		trs = table.find_elements_by_tag_name("td")
+		self.assertTrue(len(trs) > min_num_of_rows, error_msg) if error_msg is not None else self.assertTrue(
+			len(trs) > min_num_of_rows,
+			f"Table doesn't have the required number of rows. Minimum Expected: {min_num_of_rows}, Actual: {len(trs)}")
+
 	def test_can_load_cv_page(self):
 		# The user loads up the website and goes to the cv section
 		self.browser.get(f'{self.live_server_url}/cv')
@@ -22,13 +29,13 @@ class LoadCVTest(LiveServerTestCase):
 		# Check that CV is mentioned in the title
 		self.assertIn('CV', self.browser.title)
 
-		# The user sees that there is a table that contains basic information
+		# The user sees that there is a table that contains basic information, specifically my name and contact email
 		try:
 			basic_information = self.browser.find_element_by_id("table_BasicInfo")
 		except NoSuchElementException as nsee:
 			self.fail(nsee.msg)
 
-		# Check that the table contains at least my name and contact email
+		## Check that the table contains at least my name and contact email
 		details = basic_information.find_elements_by_tag_name("td")
 
 		if len(details) < 2:
@@ -37,28 +44,39 @@ class LoadCVTest(LiveServerTestCase):
 		values_to_test = ["Johnathon Kemp", "kemp133@googlemail.com"]
 
 		for val in values_to_test:
-			self.assertIn(val, (data.text for data in details),
+			self.assertIn(val, [data.text for data in details],
 						  f"The following value could not be found in the table: {val}\nValues searched: {values_to_test}")
 
 		# The user finishes looking through the basic information, and scrolls down to begin looking at the past
-		# education history
+		# education history. They notice a section for GCSE results, and another for A-Level results
 
-		# Check that there are two tables present (one for GCSE, the other for A-Level)
+		## Check that there are two tables present (one for GCSE, the other for A-Level)
 		past_education_tables = self.browser.find_elements_by_class_name("table_PastEducation")
 		num_of_education_tables = len(past_education_tables)
 		self.assertEqual(num_of_education_tables, 2, "Number of past education tables is not equal to 2.\nActual"
-														" number of tables: " + str(num_of_education_tables))
+													 " number of tables: " + str(num_of_education_tables))
 
-		# Check that there is a table for at least Secondary and College level education
+		## Check that there is a table for at least Secondary and College level education
 		required_education_table_ids = ["id_SecondaryEducation", "id_CollegeEducation"]
-		actual_education_table_ids = (str(val.id) for val in past_education_tables)
+		actual_education_table_ids = [str(val.get_attribute("id")) for val in past_education_tables]
 
 		for val in required_education_table_ids:
 			self.assertIn(val, actual_education_table_ids,
 						  f"Required value, {val}, not found in table ids. Actual values: {actual_education_table_ids}")
 
-		# Check that both tables actually have some rows attached to them
+		## Check that both tables actually have some rows attached to them
 		for table in past_education_tables:
-			self.assertTrue(len(table.find_elements_by_tag_name("tr")) > 0, f"Given table does not have any rows: {table}")
+			self.rows_exist_in_table(table, 0, f"Given table does not have any rows: {table.get_attribute('id')}")
+
+		# The user then scrolls down again, and comes to the skills and hobby section of the CV
+		## Check that there's a table for each thing, i.e. one for skills and another for hobbies
+		skills_table = self.browser.find_element_by_id("table_Skills")
+		hobbies_table = self.browser.find_element_by_id("table_Hobbies")
+
+		## Check that, once again, these items have at least one row inside of them
+		self.rows_exist_in_table(skills_table, 0)
+		self.rows_exist_in_table(hobbies_table, 0)
+
+		# Past Experience
 
 		self.fail("Reached end of tests!")
