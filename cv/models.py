2f54b0ffd692c80d2047be87ccc28363ecc3ff98
Johnathon Kemp <kemp133@googlemail.com>
2020-09-07 21:56:11 +0100

    Modified models.py to hopefully fix an error with pythonanywhere

--- cv/models.py
+++ cv/models.py
@@ -1,9 +1,10 @@
-from django.db import models
+from django.db.models import (Model, TextChoices, CharField, ForeignKey, ManyToManyField, CASCADE, BooleanField,
+							  DateField, TextField)
 
 
 # Create your models here.
-class Subject(models.Model):
-	class Grades(models.TextChoices):
+class Subject(Model):
+	class Grades(TextChoices):
 		A_STAR = 'A*', 'A*'
 		A = 'A', 'A'
 		B = 'B', 'B'
@@ -14,60 +15,60 @@ class Subject(models.Model):
 		FAIL = 'FAIL', 'FAIL'
 		NONE = 'N/A', 'N/A'
 
-	class EducationStage(models.TextChoices):
+	class EducationStage(TextChoices):
 		GCSE = "GCSE", "GCSE"
 		A_LEVEL = "A_LEVEL", "A Level"
 		HIGHER = "HIGHER", "Higher Education"
 		NONE = "NONE", "N/a"
 
-	name = models.CharField(max_length=64)
-	stage = models.CharField(max_length=8, choices=EducationStage.choices, default=EducationStage.NONE)
-	predicted_grade = models.CharField(max_length=4, choices=Grades.choices, default=Grades.NONE)
-	actual_grade = models.CharField(max_length=4, choices=Grades.choices, default=Grades.NONE)
-	active = models.BooleanField(default=True)
+	name = CharField(max_length=64)
+	stage = CharField(max_length=8, choices=EducationStage.choices, default=EducationStage.NONE)
+	predicted_grade = CharField(max_length=4, choices=Grades.choices, default=Grades.NONE)
+	actual_grade = CharField(max_length=4, choices=Grades.choices, default=Grades.NONE)
+	active = BooleanField(default=True)
 
 
-class Institution(models.Model):
-	name = models.CharField(max_length=256)
-	address_line_1 = models.CharField(max_length=256)
-	address_line_2 = models.CharField(max_length=256)
-	address_line_3 = models.CharField(max_length=256, blank=True)
-	address_line_4 = models.CharField(max_length=256)
-	post_code = models.CharField(max_length=8)
+class Institution(Model):
+	name = CharField(max_length=256)
+	address_line_1 = CharField(max_length=256)
+	address_line_2 = CharField(max_length=256)
+	address_line_3 = CharField(max_length=256, blank=True)
+	address_line_4 = CharField(max_length=256)
+	post_code = CharField(max_length=8)
 
 
-class Education(models.Model):
-	institution = models.ForeignKey(Institution, models.CASCADE)
-	subject = models.ManyToManyField(Subject, related_name="education_institute")
+class Education(Model):
+	institution = ForeignKey(Institution, CASCADE)
+	subject = ManyToManyField(Subject, related_name="education_institute")
 
 
-class BasicInformation(models.Model):
-	information_name = models.CharField(max_length=256, blank=False)
-	information_value = models.CharField(max_length=256, blank=False)
-	active = models.BooleanField(default=True)
+class BasicInformation(Model):
+	information_name = CharField(max_length=256, blank=False)
+	information_value = CharField(max_length=256, blank=False)
+	active = BooleanField(default=True)
 
 
-class Skill(models.Model):
-	skill_name = models.CharField(max_length=128, blank=False)
-	skill_explanation = models.CharField(max_length=512, blank=False)
-	active = models.BooleanField(default=True)
+class Skill(Model):
+	skill_name = CharField(max_length=128, blank=False)
+	skill_explanation = CharField(max_length=512, blank=False)
+	active = BooleanField(default=True)
 
 
-class Hobby(models.Model):
-	hobby_name = models.CharField(max_length=128, blank=False)
-	hobby_description = models.CharField(max_length=512, blank=False)
-	active = models.BooleanField(default=True)
+class Hobby(Model):
+	hobby_name = CharField(max_length=128, blank=False)
+	hobby_description = CharField(max_length=512, blank=False)
+	active = BooleanField(default=True)
 
 
-class Experience(models.Model):
-	placement_start = models.DateField()
-	placement_end = models.DateField(blank=True, null=True)
-	notes = models.TextField()
-	active = models.BooleanField(default=True)
+class Experience(Model):
+	placement_start = DateField()
+	placement_end = DateField(blank=True, null=True)
+	notes = TextField()
+	active = BooleanField(default=True)
 
 
-class PastExperience(models.Model):
-	institution = models.ForeignKey(Institution, models.CASCADE)
-	experience = models.ManyToManyField(Experience, related_name='past_experience')
+class PastExperience(Model):
+	institution = ForeignKey(Institution, CASCADE)
+	experience = ManyToManyField(Experience, related_name='past_experience')
 
 
